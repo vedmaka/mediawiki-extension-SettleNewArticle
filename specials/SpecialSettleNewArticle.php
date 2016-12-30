0ed300c209aea326e4e9bda147494b9fc8fdeb47
vedmaka <god.vedmaka@gmail.com>
2016-12-31 01:32:55 +0300

    init2

--- specials/SpecialSettleNewArticle.php
+++ specials/SpecialSettleNewArticle.php
@@ -1,6 +1,12 @@
 <?php
 
+/**
+ *
+ * Class SpecialSettleNewArticle
+ */
 class SpecialSettleNewArticle extends SpecialPage {
+
+    private $templateParser;
 
     public function __construct($name = '', $restriction = '', $listed = true, $function = false, $file = '', $includable = false)
     {
@@ -9,7 +15,14 @@ class SpecialSettleNewArticle extends SpecialPage {
 
     public function execute($subPage)
     {
+        $this->templateParser = new TemplateParser( dirname(__FILE__).'/../templates/', true );
 
+        $data = array(
+
+        );
+
+        $html = $this->templateParser->processTemplate('main', $data);
+        $this->getOutput()->addHTML($html);
     }
 
 }
