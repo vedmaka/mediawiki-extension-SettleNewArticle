fdf53c41400d89efec5d35ef062d6f5e61d75dff
vedmaka <god.vedmaka@gmail.com>
2016-12-31 01:05:41 +0300

    init

---
+++ specials/SpecialSettleNewArticle.php
@@ -0,0 +1,15 @@
+<?php
+
+class SpecialSettleNewArticle extends SpecialPage {
+
+    public function __construct($name = '', $restriction = '', $listed = true, $function = false, $file = '', $includable = false)
+    {
+        parent::__construct('SettleNewArticle');
+    }
+
+    public function execute($subPage)
+    {
+
+    }
+
+}
